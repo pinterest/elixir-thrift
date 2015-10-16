9c3871cc9568eaf23c701ae11dece7bb5790b8ab
Jon Parise <jon@pinterest.com>
2015-10-16 09:38:11 -0700

    Release version 1.0.0.

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Thrift.Mixfile do
 
   def project do
     [app: :thrift,
-     version: "0.0.1",
+     version: "1.0.0",
      description: "Thrift utilities for Elixir",
      elixir: "~> 1.0",
      erlc_paths: ["ext/thrift/lib/erl/src"],

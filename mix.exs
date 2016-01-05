0122ebe51a2db183661541caf6451ea351d1030b
ed t <edwardt.tril@gmail.com>
2016-01-04 20:33:19 -0800

    Various build process enhancement * add travisci support * run code coverage post result to coverall.io * add project info for hex package * add Build and code coverage badge, compile only

--- mix.exs
+++ mix.exs
@@ -6,12 +6,43 @@ defmodule Thrift.Mixfile do
      version: "1.0.0",
      description: "Thrift utilities for Elixir",
      elixir: "~> 1.0",
+     description: description,
+     source_url: project_url,
+     homepage_url: project_url,
+     package: package,
+     deps: deps,
      erlc_paths: ["ext/thrift/lib/erl/src"],
      erlc_include_path: "ext/thrift/lib/erl/include",
-     compilers: [:erlang, :elixir, :app]]
+     compilers: [:erlang, :elixir, :app],
+     test_coverage: [tool: ExCoveralls],
+     preferred_cli_env: ["coveralls": :test, "coveralls.detail": :test, "coveralls.post": :test],
+     ]
   end
 
   def application do
-    []
+     []
+  end
+
+  defp description do
+     """
+     elixir-thrift contains a set of useful utilities to work with Thrift in Elixir. Erlang Thrift runtime library is embedded.
+     """
+  end
+
+  defp project_url do
+    """
+     https://github.com/pinterest/elixir-thrift
+     """
+  end
+
+  defp deps do
+     [{:excoveralls, github: "parroty/excoveralls", tag: "v0.4.3", override: true, only: :test}]
+  end
+
+  defp package do
+     [files: ["ext", "lib", "mix.exs", "README.md", "LICENSE"],
+      maintainers: ["Jon Parise", "Steve Cohen"],
+      licenses: ["Apache 2.0"],
+      links: %{"GitHub" => project_url}]
   end
 end

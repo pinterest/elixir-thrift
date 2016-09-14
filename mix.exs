0f0ec7a214feb17885651622724ed29648a57996
Jon Parise <jon@pinterest.com>
2016-09-14 11:42:56 -0700

    Upgrade to credo 0.4.11
    No new issues were flagged as a result of this upgrade.

--- mix.exs
+++ mix.exs
@@ -41,7 +41,7 @@ defmodule Thrift.Mixfile do
      [{:ex_doc, "~> 0.12.0", only: :dev},
       {:earmark, "~> 0.2.1", only: :dev},
       {:excoveralls, "~> 0.5.4", only: :test},
-      {:credo, "~> 0.4.5", only: [:dev, :test]},
+      {:credo, "~> 0.4.11", only: [:dev, :test]},
       {:dialyze, "~> 0.2.0", only: [:dev, :test]}
      ]
   end

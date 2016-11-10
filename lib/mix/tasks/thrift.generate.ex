883172ea0b090f2dc4be83a2b8f2f16d9f8c2d6c
Preston Guillory <preston@pinterest.com>
2016-11-10 14:16:28 -0800

    Rename --lib-dir to --output-dir

--- lib/mix/tasks/thrift.generate.ex
+++ lib/mix/tasks/thrift.generate.ex
@@ -7,17 +7,17 @@ defmodule Mix.Tasks.Thrift.Generate do
   ## Command line options
 
     * `--thrift-dir` - Directory to scan for .thrift files. (Default: ./thrift)
-    * `--lib-dir` - Directory under which to place generated .ex files. (Default: ./lib)
+    * `--output-dir` - Directory under which to place generated .ex files. (Default: ./lib)
   """
 
   def run(args) do
     {opts, _, _} = OptionParser.parse(args)
 
     thrift_dir = Keyword.get(opts, :thrift_dir, "thrift")
-    lib_dir = Keyword.get(opts, :lib_dir, "lib")
+    output_dir = Keyword.get(opts, :output_dir, "lib")
 
     for thrift_file <- Mix.Utils.extract_files([thrift_dir], "*.thrift") do
-      for output_file <- Thrift.Generator.Models.generate!(thrift_file, lib_dir) do
+      for output_file <- Thrift.Generator.Models.generate!(thrift_file, output_dir) do
         Mix.shell.info "Generated #{output_file}"
       end
     end

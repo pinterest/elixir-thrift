c5595ef1abc96f5848c9792af8d92d097312563c
Preston Guillory <preston@pinterest.com>
2016-11-16 17:17:54 -0800

    Add deserialization tests

--- lib/generator/binary_protocol.ex
+++ lib/generator/binary_protocol.ex
@@ -109,7 +109,7 @@ defmodule Thrift.Generator.Models.BinaryProtocol do
 
   def field_deserializer(:string, field, name, _file_group) do
     quote do
-      defp unquote(name)(<<11, unquote(field.id)::16-signed, string_size::size(32), rest::binary>>, acc) do
+      defp unquote(name)(<<11, unquote(field.id)::16-signed, string_size::32-signed, rest::binary>>, acc) do
         <<value::binary-size(string_size), rest::binary>> = rest
         unquote(name)(rest, %{acc | unquote(field.name) => value})
       end
@@ -251,7 +251,7 @@ defmodule Thrift.Generator.Models.BinaryProtocol do
 
   def map_value_deserializer(:string, name, _file_group) do
     quote do
-      defp unquote(name)(<<string_size::size(64), rest::binary>>, key, [map | stack], remaining) do
+      defp unquote(name)(<<string_size::32-signed, rest::binary>>, key, [map | stack], remaining) do
         <<value::binary-size(string_size), rest::binary>> = rest
         unquote(name)(rest, [Map.put(map, key, value) | stack], remaining - 1)
       end
@@ -273,6 +273,29 @@ defmodule Thrift.Generator.Models.BinaryProtocol do
   end
 
 
+
+  def list_deserializer(:bool, name, _file_group) do
+    quote do
+      defp unquote(name)(<<0, rest::binary>>, [list, remaining | stack]) do
+        unquote(name)(rest, [[false | list], remaining - 1 | stack])
+      end
+      defp unquote(name)(<<1, rest::binary>>, [list, remaining | stack]) do
+        unquote(name)(rest, [[true | list], remaining - 1 | stack])
+      end
+    end
+  end
+
+  def list_deserializer(:byte, name, file_group) do
+    list_deserializer(:i8, name, file_group)
+  end
+
+  def list_deserializer(:double, name, _file_group) do
+    quote do
+      defp unquote(name)(<<element::signed-float, rest::binary>>, [list, remaining | stack]) do
+        unquote(name)(rest, [[element | list], remaining - 1 | stack])
+      end
+    end
+  end
 
   def list_deserializer(:i8, name, _file_group) do
     quote do
@@ -308,7 +331,7 @@ defmodule Thrift.Generator.Models.BinaryProtocol do
 
   def list_deserializer(:string, name, _file_group) do
     quote do
-      defp unquote(name)(rest, [list, remaining | stack]) do
+      defp unquote(name)(<<string_size::32-signed, rest::binary>>, [list, remaining | stack]) do
         <<element::binary-size(string_size), rest::binary>> = rest
         unquote(name)(rest, [[element | list], remaining - 1 | stack])
       end
@@ -328,8 +351,24 @@ defmodule Thrift.Generator.Models.BinaryProtocol do
     end
   end
 
-  def list_deserializer(struct=%Struct{}, name, file_group) do
-    dest_module = FileGroup.dest_module(file_group, struct.name)
+  def list_deserializer({:map, key_type, value_type}, name, file_group) do
+    sub_name = :"#{name}__element"
+    quote do
+      defp unquote(name)(<<unquote(type_id(key_type, file_group)),
+                           unquote(type_id(value_type, file_group)),
+                           inner_remaining::size(32),
+                           rest::binary>>,
+                         [list, remaining | stack]) do
+        unquote(sub_name)(rest, [%{}, inner_remaining, list, remaining | stack])
+      end
+      defp unquote(sub_name)(rest, [map, 0, list, remaining | stack]) do
+        unquote(name)(rest, [[map | list], remaining - 1 | stack])
+      end
+      unquote(list_deserializer(element_type, sub_name, file_group))
+    end
+  end
+
+  def list_deserializer(struct=%Struct{}, name, _file_group) do
     quote do
       defp unquote(name)(rest, [list, remaining | stack]) do
         {element, rest} = unquote(dest_module).BinaryProtocol.deserialize(rest)

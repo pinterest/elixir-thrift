4a2824a88b0cd3b5007593866774a5e92cd6c805
Preston Guillory <preston@pinterest.com>
2016-11-18 14:02:20 -0800

    Add missing field numbers

--- test/generator/models_test.exs
+++ test/generator/models_test.exs
@@ -37,8 +37,8 @@ defmodule Thrift.Generator.ModelsTest do
     1: string message,
     2: required i32 count,
     3: optional string reason
-    optional string other;
-    optional string fixed = "foo"
+    4: optional string other;
+    5: optional string fixed = "foo"
   }
   """
 

002ae40ddccab1cce5fc2b514e08a361bcf3c96d
Preston Guillory <pguillory@gmail.com>
2016-11-18 13:18:09 -0800

    Add enum 'valid?' method (#42)
    * Add enum 'member?' method
    
    This generates a member?/1 method on enum models. It tests whether a given
    number is a valid ordinal for the given enum.

--- lib/generator/models.ex
+++ lib/generator/models.ex
@@ -74,16 +74,23 @@ defmodule Thrift.Generator.Models do
   end
 
   defp generate_enum(name, enum) do
-    parts = Enum.map(enum.values, fn {key, value} ->
+    macro_defs = Enum.map(enum.values, fn {key, value} ->
       macro_name = key |> to_string |> String.downcase |> String.to_atom
       quote do
         defmacro unquote(Macro.var(macro_name, nil)), do: unquote(value)
       end
     end)
+    member_defs = Enum.map(enum.values, fn {_key, value} ->
+      quote do
+        def member?(unquote(value)), do: true
+      end
+    end)
     quote do
       defmodule unquote(name) do
         @moduledoc unquote("Auto-generated Thrift enum #{enum.name}")
-        unquote_splicing(parts)
+        unquote_splicing(macro_defs)
+        unquote_splicing(member_defs)
+        def member?(_), do: false
       end
     end
   end

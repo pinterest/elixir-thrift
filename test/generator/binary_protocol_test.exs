2c5148551d7938f0f96d8825a40e97789b806669
Preston Guillory <preston@pinterest.com>
2016-11-21 17:09:53 -0800

    Convert Generator.BinaryProtocolTest to ThriftTestCase

--- test/generator/binary_protocol_test.exs
+++ test/generator/binary_protocol_test.exs
@@ -1,46 +1,19 @@
 defmodule Thrift.Generator.BinaryProtocolTest do
   use ThriftTestCase, cleanup: false
-  # use ExUnit.Case, async: true
 
-  import Thrift.Generator.Models
+  @thrift_file name: "struct.thrift", contents: """
+  struct MyStruct {
+    1: optional string name = "asdf";
+    2: optional byte num8 = 8;
+    3: optional i16 num16 = 16;
+    4: optional i32 num32 = 32;
+    5: optional i64 num64 = 64;
+    6: optional bool b1;
+  }
+  """
 
-  setup %{test: test} do
-    dir = [System.tmp_dir!, __MODULE__, test]
-    |> Enum.map(&to_string/1)
-    |> Path.join
-
-    File.rm_rf!(dir)
-    File.mkdir_p!(dir)
-    on_exit fn ->
-      # File.rm_rf!(dir)
-      :ok
-    end
-    {:ok, dir: dir}
-  end
-
-  def load_generated_file(filename) do
-    IO.puts filename
-    # File.read!(filename) |> IO.puts
-    Code.eval_file(filename)
-  end
-
-  @tag pending: true
-  test "generating struct", %{dir: dir} do
-    File.write! "#{dir}/test.thrift", """
-      struct MyStruct {
-        1: optional string name = "asdf";
-        2: optional byte num8 = 8;
-        3: optional i16 num16 = 16;
-        4: optional i32 num32 = 32;
-        5: optional i64 num64 = 64;
-        6: optional bool b1;
-      }
-      """
-
-    generate! "#{dir}/test.thrift", dir
-    load_generated_file "#{dir}/my_struct.ex"
-
-    struct = MyStruct.new
+  thrift_test "generating struct" do
+    struct = %MyStruct{}
     binary = MyStruct.BinaryProtocol.serialize(:struct, struct) |> :erlang.iolist_to_binary
     assert binary == <<
       # string field 1 = "asdf"
@@ -67,35 +40,28 @@ defmodule Thrift.Generator.BinaryProtocolTest do
     assert {^struct, ""} = MyStruct.BinaryProtocol.deserialize(binary)
   end
 
-  @tag pending: true
-  test "lists", %{dir: dir} do
-    File.write! "#{dir}/test.thrift", """
-      namespace elixir #{__MODULE__};
-      struct StructElement {
-        1: optional i32 num;
-      }
-      struct Lists {
-        1: optional list<bool> list_of_bool;
-        2: optional list<byte> list_of_byte;
-        3: optional list<double> list_of_double;
-        4: optional list<i16> list_of_i16;
-        5: optional list<i32> list_of_i32;
-        6: optional list<i64> list_of_i64;
-        7: optional list<string> list_of_string;
-        # 8: optional list<StructElement> list_of_structs;
-        9: optional list<map<string, i32>> list_of_maps;
-        10: optional list<set<string>> list_of_sets;
-        11: optional list<list<i32>> list_of_lists_of_i32;
-        12: optional list<list<list<i32>>> list_of_lists_of_lists_of_i32;
-      }
-      """
-    generate! "#{dir}/test.thrift", dir
-    load_generated_file "#{dir}/thrift/generator/binary_protocol_test/lists.ex"
-    load_generated_file "#{dir}/thrift/generator/binary_protocol_test/struct_element.ex"
-    alias __MODULE__.Lists
-    alias __MODULE__.StructElement
+  @thrift_file name: "lists.thrift", contents: """
+  struct StructElement {
+    1: optional i32 num;
+  }
+  struct Lists {
+    1: optional list<bool> list_of_bool;
+    2: optional list<byte> list_of_byte;
+    3: optional list<double> list_of_double;
+    4: optional list<i16> list_of_i16;
+    5: optional list<i32> list_of_i32;
+    6: optional list<i64> list_of_i64;
+    7: optional list<string> list_of_string;
+    # 8: optional list<StructElement> list_of_structs;
+    9: optional list<map<string, i32>> list_of_maps;
+    10: optional list<set<string>> list_of_sets;
+    11: optional list<list<i32>> list_of_lists_of_i32;
+    12: optional list<list<list<i32>>> list_of_lists_of_lists_of_i32;
+  }
+  """
 
-    struct = %{Lists.new |
+  thrift_test "lists" do
+    struct = %Lists{
       list_of_bool: [true, false, true],
       list_of_byte: [3, 4, 5, 0],
       list_of_double: [0.0, 1.0, 2.0],

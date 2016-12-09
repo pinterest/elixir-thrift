4c71e984e7e94c4e4ee918ce37bba7460474d5b3
Jon Parise <jon@indelible.org>
2016-12-09 10:12:01 -0800

    Fix Elixir 1.4 "function call ambiguity" warnings (#65)

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule Thrift.Mixfile do
     [app: :thrift,
      version: @version,
      elixir: "~> 1.2",
-     deps: deps,
+     deps: deps(),
 
      # Build Environment
      erlc_paths: erlc_paths(Mix.env),
@@ -25,8 +25,8 @@ defmodule Thrift.Mixfile do
      homepage_url: @project_url,
 
      # Hex
-     description: description,
-     package: package,
+     description: description(),
+     package: package(),
 
      # Docs
      name: "Thrift",

ad83e48cb92962f1d3163a911256cc51549dc58d
Steve Cohen <steve@pinterest.com>
2016-12-29 08:28:38 -0800

    Behaviour module generation
    For the server side, I imagine that the user would specify a handler
    module and pass it into the service. Then it would be called with the
    appropriate arguments.
    I thought it might be nice to have a behaviour for these modules that
    users could be implement.
    
    Behavoiurs here are fully typed with the correct success types.

--- lib/thrift.ex
+++ lib/thrift.ex
@@ -7,4 +7,10 @@ defmodule Thrift do
 
     * `Thrift.Parser` - functions for parsing `.thift` schema files
   """
+
+  @type i8   :: (-128..127)
+  @type double :: float()
+  @type i16 :: (-32_768..32_767)
+  @type i32 :: (-2_147_483_648..2_147_483_647)
+  @type i64 :: (-9_223_372_036_854_775_808..9_223_372_036_854_775_807)
 end
---
+++ test/generator/behaviour_test.exs
@@ -0,0 +1,33 @@
+defmodule BehaviourTest do
+  use ThriftTestCase
+
+  @thrift_file name: "behaviour.thrift", contents: """
+  struct S {
+    1: string username
+  }
+
+  service BehaviourService {
+    void ping(1: i64 my_int),
+    void my_bool(1: bool my_bool),
+    void numbers(1: byte b, 2: i16 i, 3: i32 eye32, 4: i64 eye64, 5: double dub),
+    void my_set(1: set<string> my_set),
+    void my_list(1: list<string> my_string),
+    void my_map(1: map<string, string> my_map)
+    map<string, bool> my_map2(1: map<string, map<string, string>> my_map)
+    void struct_param(1: S my_struct)
+  }
+  """
+
+  thrift_test "that behaviours exist" do
+    behaviour_specs = Behaviour.behaviour_info(:callbacks)
+
+    assert {:ping, 1} in behaviour_specs
+    assert {:my_set, 1} in behaviour_specs
+    assert {:my_list, 1} in behaviour_specs
+    assert {:my_map, 1} in behaviour_specs
+    assert {:my_map2, 1} in behaviour_specs
+    assert {:numbers, 5} in behaviour_specs
+    assert {:my_bool, 1} in behaviour_specs
+    assert {:struct_param, 1} in behaviour_specs
+  end
+end

4d6139c45a4c4f00200657973b6dcebf2078e73b
Preston Guillory <preston@pinterest.com>
2016-12-29 12:27:47 -0800

    Remove UnionSerializationBenchmark
    The changes to ParserUtils were fumbled and broke BinaryProtocolBenchmark,
    which is the more valuable benchmark. UnionSerializationBenchmark was used to
    check the performance impact of a particular commit and isn't really worth
    maintaining.

--- test/fixtures/app/thrift/simple.thrift
+++ test/fixtures/app/thrift/simple.thrift
@@ -1,11 +1,5 @@
 include "shared.thrift"
 
-union TestUnion {
-  1: string s,
-  2: i64 i,
-  3: map<string, string> m
-}
-
 struct User {
   1: bool is_evil,
   2: i64 user_id,
@@ -18,7 +12,6 @@ struct User {
   9: map<byte, string> my_map,
   10: set<i32> blocked_user_ids,
   11: optional list<i32> optional_integers,
-  12: TestUnion u
 }
 
 struct Nesting {

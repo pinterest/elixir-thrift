4b0503a44a0d4826442e873accc39b3a763601c2
Jon Parise <jon@pinterest.com>
2016-12-30 09:23:10 -0800

    Strictly match just true and false for :bool

--- lib/thrift/protocols/binary.ex
+++ lib/thrift/protocols/binary.ex
@@ -49,9 +49,8 @@ defmodule Thrift.Protocols.Binary do
   def serialize(_, nil) do
     []
   end
-  def serialize(:bool, nil),   do: <<0::8-signed>>
   def serialize(:bool, false), do: <<0::8-signed>>
-  def serialize(:bool, _),     do: <<1::8-signed>>
+  def serialize(:bool, true),  do: <<1::8-signed>>
   def serialize(:i8, value) do
     <<value::8-signed>>
   end

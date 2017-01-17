9d2ce55620a44e3cf8dd502d12f16f9b9273df75
Jon Parise <jon@pinterest.com>
2017-01-17 14:18:58 -0800

    Reintroduce the 'thrift.generate' mix task (#186)
    My previous attempt to consolidate these two mix tasks into one was
    unsuccessful due to the way mix tasks receive their command line
    arguments. Specifically, we don't have a good way to differentiate
    between e.g. a `mix test` command line containing test script filenames
    and a `mix compile.thrift` command line naming Thrift schema files. When
    the `:thrift` compiler is included, it will also run in the `mix test`
    task flow and get confused about its inputs.
    
    We now have two distinct and purpose-built mix tasks:
    
     - `compile.thrift` - intended for use in the Mix.compilers list
     - `thrift.generate` - intended for interactive command-line use

---
+++ lib/mix/tasks/thrift.generate.ex
@@ -0,0 +1,67 @@
+defmodule Mix.Tasks.Thrift.Generate do
+  use Mix.Task
+
+  @shortdoc "Generate Elixir source files from Thrift schema files"
+
+  @moduledoc """
+  Generate Elixir source files from Thrift schema files (`.thrift`).
+
+  A list of files can be given after the task name in order to select the
+  specific Thrift schema files to parse:
+
+      mix thrift.generate file1.thrift file2.thrift
+
+  ## Command line options
+
+    * `-o` `--out` - set the output directory, overriding the `:thrift_output`
+      configuration value
+    * `-v` `--verbose` - enable verbose task logging
+
+  ## Configuration
+
+    * `:thrift_output` - output directory into which the generated Elixir
+      source file will be generated. Defaults to `"lib"`.
+  """
+
+  @spec run(OptionParser.argv) :: :ok
+  def run(args) do
+    {opts, files} = OptionParser.parse!(args,
+      aliases: [o: :out, v: :verbose],
+      switches: [out: :string, verbose: :boolean])
+
+    config     = Mix.Project.config
+    output_dir = opts[:out] || Keyword.get(config, :thrift_output, "lib")
+
+    unless Enum.empty?(files) do
+      File.mkdir_p!(output_dir)
+      Enum.each(files, &generate!(&1, output_dir, opts))
+    end
+  end
+
+  defp parse!(thrift_file) do
+    try do
+      Thrift.Parser.parse_file(thrift_file)
+    rescue
+      e ->
+        Mix.raise "#{thrift_file}: #{Exception.message(e)}"
+    end
+  end
+
+  defp generate!(thrift_file, output_dir, opts) do
+    Mix.shell.info "Parsing #{thrift_file}"
+
+    generated_files =
+      thrift_file
+      |> parse!
+      |> Thrift.Generator.generate!(output_dir)
+
+    if opts[:verbose] do
+      generated_files
+      |> Enum.uniq
+      |> Enum.sort
+      |> Enum.each(fn file ->
+        Mix.shell.info "Wrote #{file}"
+      end)
+    end
+  end
+end
---
+++ test/mix/tasks/thrift.generate_test.exs
@@ -0,0 +1,62 @@
+defmodule Mix.Tasks.Thrift.GenerateTest do
+  use ExUnit.Case
+  import ExUnit.CaptureIO
+
+  @project_root Path.expand("../../../", __DIR__)
+  @fixture_project Path.join(@project_root, "test/fixtures/app")
+
+  setup do
+    in_fixture(fn -> File.rm_rf!("lib") end)
+    :ok
+  end
+
+  test "not specifying any Thrift files" do
+    in_fixture fn ->
+      assert run([]) == ""
+    end
+  end
+
+  test "specifying multiple Thrift files" do
+    in_fixture fn ->
+      output = run(["thrift/simple.thrift", "thrift/tutorial.thrift"])
+      assert output =~ "Parsing thrift/simple.thrift"
+      assert output =~ "Parsing thrift/tutorial.thrift"
+      assert File.exists?("lib/shared/shared_struct.ex")
+      assert File.exists?("lib/tutorial/calculator.ex")
+    end
+  end
+
+  test "specifying a non-existent Thrift file" do
+    in_fixture fn ->
+      assert_raise Mix.Error, ~r/could not read file/, fn ->
+        run(["missing.thrift"])
+      end
+    end
+  end
+
+  test "specifying an invalid Thrift file" do
+    in_fixture fn ->
+      assert_raise Mix.Error, ~r/Error parsing/, fn ->
+        run([__ENV__.file])
+      end
+    end
+  end
+
+  test "specifying an alternate output directory (--out)" do
+    in_fixture fn ->
+      run(["--out", "thrift_lib", "thrift/simple.thrift"])
+      assert File.exists?("thrift_lib/shared/shared_struct.ex")
+      File.rm_rf!("thrift_lib")
+    end
+  end
+
+  defp run(args) when is_list(args), do: run(:stdio, args)
+  defp run(device, args) when device in [:stdio, :stderr] and is_list(args) do
+    args = ["--verbose" | args]
+    capture_io(device, fn -> Mix.Tasks.Thrift.Generate.run(args) end)
+  end
+
+  defp in_fixture(fun) do
+    File.cd!(@fixture_project, fun)
+  end
+end

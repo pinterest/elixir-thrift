870de631e75e06c833970b0b8f7137bef95444cc
Dan Swain <dan.t.swain@gmail.com>
2017-01-26 18:30:03 -0500

    Rename/move default_value to quoted_value
    Since it's now used in more than one place.  I'm not convinced that
    Utils is the perfect spot for it but nothing else made sence either.

--- lib/thrift/generator/constant_generator.ex
+++ lib/thrift/generator/constant_generator.ex
@@ -2,12 +2,11 @@ defmodule Thrift.Generator.ConstantGenerator do
   @moduledoc false
 
   alias Thrift.Generator.Utils
-  alias Thrift.Generator.StructGenerator
 
   def generate(full_name, constants, schema) do
     constant_defs = Enum.map(constants, fn(constant) ->
       name = Utils.underscore(constant.name)
-      value = StructGenerator.default_value(constant.value, constant.type, schema)
+      value = Utils.quote_value(constant.value, constant.type, schema)
       quote do
         def unquote(name)() do
           unquote(value)
--- lib/thrift/generator/struct_generator.ex
+++ lib/thrift/generator/struct_generator.ex
@@ -3,20 +3,18 @@ defmodule Thrift.Generator.StructGenerator do
   alias Thrift.Generator.Utils
   alias Thrift.Parser.FileGroup
   alias Thrift.Parser.Models.{
-    Constant,
     Exception,
     Field,
     Struct,
     TypeRef,
     TEnum,
     Union,
-    ValueRef,
   }
 
   def generate(label, schema, name, struct) when label in [:struct, :union, :exception] do
     struct_parts = Enum.map(struct.fields, fn
       %Field{name: name, type: type, default: default} ->
-        {name, default_value(default, type, schema)}
+        {name, Utils.quote_value(default, type, schema)}
     end)
 
     binary_protocol_defs = [
@@ -59,76 +57,6 @@ defmodule Thrift.Generator.StructGenerator do
         end
       end
     end
-  end
-
-  def default_value(%ValueRef{} = ref, type, schema) do
-    value = FileGroup.resolve(schema.file_group, ref)
-    default_value(value, type, schema)
-  end
-  def default_value(value, %TypeRef{} = ref, schema) do
-    type = FileGroup.resolve(schema.file_group, ref)
-    default_value(value, type, schema)
-  end
-  def default_value(%Constant{type: %TypeRef{} = ref} = constant, type, schema) do
-    constant = %Constant{constant | type: FileGroup.resolve(schema.file_group, ref)}
-    default_value(constant, type, schema)
-  end
-  def default_value(%Constant{type: type, value: value}, type, schema) do
-    default_value(value, type, schema)
-  end
-  def default_value(nil, %TEnum{values: [{_, value} | _]}, _schema) do
-    value
-  end
-  def default_value(nil, _type, _schema) do
-    nil
-  end
-  def default_value(value, :bool, _schema) when is_boolean(value), do: value
-  def default_value(value, :byte, _schema) when is_integer(value), do: value
-  def default_value(value, :double, _schema) when is_number(value), do: value
-  def default_value(value, :i8, _schema) when is_integer(value), do: value
-  def default_value(value, :i16, _schema) when is_integer(value), do: value
-  def default_value(value, :i32, _schema) when is_integer(value), do: value
-  def default_value(value, :i64, _schema) when is_integer(value), do: value
-  def default_value(value, :string, _schema) when is_binary(value), do: value
-  def default_value(value, :string, _schema) when is_list(value) do
-    List.to_string(value)
-  end
-  def default_value(value, :binary, schema) do
-    default_value(value, :string, schema)
-  end
-  def default_value(values, %Struct{fields: fields} = struct, schema) when is_list(values) do
-    struct_module = FileGroup.dest_module(schema.file_group, struct)
-    types = Map.new(fields, fn %Field{name: name, type: type} ->
-      {name, type}
-    end)
-    defaults = Enum.map(fields, fn %Field{name: name, type: type, default: default} ->
-      {name, default_value(default, type, schema)}
-    end)
-    values = Enum.map(values, fn {name_charlist, value} ->
-      name = List.to_string(name_charlist) |> String.to_existing_atom
-      type = Map.fetch!(types, name)
-      {name, default_value(value, type, schema)}
-    end)
-    quote do
-      %{unquote_splicing([{:__struct__, struct_module} | Keyword.new(defaults ++ values)])}
-    end
-  end
-  def default_value(%{} = value, {:map, {key_type, value_type}}, schema) do
-    Map.new(value, fn {key, value} ->
-      {
-        default_value(key, key_type, schema),
-        default_value(value, value_type, schema),
-      }
-    end)
-  end
-  def default_value(%MapSet{} = set, {:set, type}, schema) do
-    values = Enum.map(set, &default_value(&1, type, schema))
-    quote do
-      MapSet.new(unquote(values))
-    end
-  end
-  def default_value(list, {:list, type}, schema) when is_list(list) do
-    Enum.map(list, &default_value(&1, type, schema))
   end
 
   def to_thrift(base_type, _file_group) when is_atom(base_type) do

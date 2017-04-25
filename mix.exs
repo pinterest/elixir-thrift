0432784b11c14e79635bd0d87c6befc0a32cd12f
Jon Parise <jon@pinterest.com>
2017-04-25 15:31:58 -0700

    Upgrade to credo 0.7.3
    This also updates the .credo.exs configuration file and disables the
    `ParenthesesOnZeroArityDefs` check because it crashes on our code base
    (for some to-be-determined reason).

--- mix.exs
+++ mix.exs
@@ -70,7 +70,7 @@ defmodule Thrift.Mixfile do
   defp deps do
      [{:ex_doc, "~> 0.14", only: :dev},
       {:excoveralls, "~> 0.6", only: [:dev, :test]},
-      {:credo, "~> 0.6", only: [:dev, :test]},
+      {:credo, "~> 0.7", only: [:dev, :test]},
       {:dialyxir, "~> 0.5", only: :dev, runtime: false},
       {:benchfella, "~> 0.3", only: [:dev, :test]},
       {:connection, "~> 1.0"},

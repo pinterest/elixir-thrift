d24f0b0613a3600663e990f9c76228b9333217e7
Michael Oliver <moliver@pinterest.com>
2018-05-11 00:07:30 -0700

    Write large number with underscores

--- lib/thrift/nan.ex
+++ lib/thrift/nan.ex
@@ -4,7 +4,7 @@ defmodule Thrift.NaN do
   """
 
   @type t :: %Thrift.NaN{sign: 0 | 1,
-                         fraction: (1..4503599627370495)  # 2^52 - 1
+                         fraction: (1..4_503_599_627_370_495)  # 2^52 - 1
                         }
   defstruct sign: nil,
             fraction: nil

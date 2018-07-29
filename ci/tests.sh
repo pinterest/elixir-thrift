9e9d18716b6c98e240897b3586025643e63ed4ce
Michael Oliver <mikemboliver@gmail.com>
2018-07-29 16:20:12 -0700

    Add formatter check to CI

--- ci/tests.sh
+++ ci/tests.sh
@@ -2,13 +2,21 @@
 
 # Runs the test suite for the travis build
 #
+# If CHECK_FORMAT is true, then we run the mix formatter to ensure that
+# all files are correctly formatted.
 # If COVERALLS is true, then we report test coverage to coveralls.
 #
 # This script could be used for local testing as long as COVERALLS is not set.
 
 export MIX_ENV=test
 
-if [ "$COVERALLS" = true  ]
+if [ "$CHECK_FORMAT" = true ]
+then
+  echo "Checking that files are formatted"
+  mix format --check-formatted
+fi
+
+if [ "$COVERALLS" = true ]
 then
   echo "Coveralls will be reported"
   TEST_COMMAND=coveralls.travis

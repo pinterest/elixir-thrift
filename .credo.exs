d2d35e28f4b8b722eeced3cad1059e41cc3ef953
Jon Parise <jon@pinterest.com>
2018-07-29 11:51:19 -0700

    Upgrade to credo 0.10 (#357)
    Also exclude the test/fixtures/app/lib/ path because it contains
    generated code.

--- .credo.exs
+++ .credo.exs
@@ -2,7 +2,7 @@
   %{name: "default",
     files: %{
       included: ["lib/", "src/", "test/"],
-      excluded: [~r"/_build/", ~r"/deps/"]
+      excluded: [~r"/_build/", ~r"/deps/", "test/fixtures/app/lib/"]
     },
     requires: [],
     check_for_updates: false,

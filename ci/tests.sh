f5da5e463fda684474315fb18899eb960ac9f58c
Michael Oliver <mikemboliver@gmail.com>
2018-07-29 17:04:07 -0700

    Rename env variable, better formatter message

--- ci/tests.sh
+++ ci/tests.sh
@@ -2,16 +2,16 @@
 
 # Runs the test suite for the travis build
 #
-# If EXTRA_CI_CHECKS is true, then we report test coverage to coveralls, and run
+# If EXTRA_CHECKS is true, then we report test coverage to coveralls, and run
 # the mix formatter to ensure all files are formatted.
 #
-# This script could be used for local testing as long as EXTRA_CI_CHECKS is not set.
+# This script could be used for local testing as long as EXTRA_CHECKS is not set.
 
 export MIX_ENV=test
 
-if [ "$EXTRA_CI_CHECKS" = true  ]
+if [ "$EXTRA_CHECKS" = true  ]
 then
-  echo "Checking that all code is formatted"
+  echo "Checking code format (`mix format --check-formatted`)"
   mix format --check-formatted
   echo "Coveralls will be reported"
   mix coveralls.travis

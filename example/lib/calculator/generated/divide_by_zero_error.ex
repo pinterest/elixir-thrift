050e691be263a0fca81167fcc5e4a36bfd446a2f
Jon Parise <jon@pinterest.com>
2018-09-02 10:02:07 -0700

    Regenerate the example's generated files
    This includes the `@moduledoc false` changes we recently made to the
    Thrift generators.
    
    The rest of the formatting changes come from regenerating these files
    under Elixir 1.7.

--- example/lib/calculator/generated/divide_by_zero_error.ex
+++ example/lib/calculator/generated/divide_by_zero_error.ex
@@ -1,62 +1,44 @@
 defmodule(Calculator.Generated.DivideByZeroError) do
+  @moduledoc(false)
   _ = "Auto-generated Thrift exception calculator.DivideByZeroError"
   _ = "1: string message"
   defexception(message: nil)
-  @type t :: %__MODULE__{}
+  @type(t :: %__MODULE__{})
   def(new) do
     %__MODULE__{}
   end
-
   defmodule(BinaryProtocol) do
-    @moduledoc false
+    @moduledoc(false)
     def(deserialize(binary)) do
       deserialize(binary, %Calculator.Generated.DivideByZeroError{})
     end
-
     defp(deserialize(<<0, rest::binary>>, %Calculator.Generated.DivideByZeroError{} = acc)) do
       {acc, rest}
     end
-
-    defp(
-      deserialize(
-        <<11, 1::16-signed, string_size::32-signed, value::binary-size(string_size),
-          rest::binary>>,
-        acc
-      )
-    ) do
+    defp(deserialize(<<11, 1::16-signed, string_size::32-signed, value::binary-size(string_size), rest::binary>>, acc)) do
       deserialize(rest, %{acc | message: value})
     end
-
     defp(deserialize(<<field_type, _id::16-signed, rest::binary>>, acc)) do
       rest |> Thrift.Protocol.Binary.skip_field(field_type) |> deserialize(acc)
     end
-
     defp(deserialize(_, _)) do
       :error
     end
-
     def(serialize(%Calculator.Generated.DivideByZeroError{message: message})) do
-      [
-        case(message) do
-          nil ->
-            <<>>
-
-          _ ->
-            [<<11, 1::16-signed, byte_size(message)::32-signed>> | message]
-        end
-        | <<0>>
-      ]
+      [case(message) do
+        nil ->
+          <<>>
+        _ ->
+          [<<11, 1::16-signed, byte_size(message)::32-signed>> | message]
+      end | <<0>>]
     end
   end
-
   def(serialize(struct)) do
     BinaryProtocol.serialize(struct)
   end
-
   def(serialize(struct, :binary)) do
     BinaryProtocol.serialize(struct)
   end
-
   def(deserialize(binary)) do
     BinaryProtocol.deserialize(binary)
   end
--- example/lib/calculator/generated/vector_product_type.ex
+++ example/lib/calculator/generated/vector_product_type.ex
@@ -1,75 +1,58 @@
 defmodule(Calculator.Generated.VectorProductType) do
-  @moduledoc "Auto-generated Thrift enum calculator.VectorProductType"
+  @moduledoc(false)
   defmacro(unquote(:dot_product)()) do
     1
   end
-
   defmacro(unquote(:cross_product)()) do
     2
   end
-
   def(value_to_name(1)) do
     {:ok, :dot_product}
   end
-
   def(value_to_name(2)) do
     {:ok, :cross_product}
   end
-
   def(value_to_name(v)) do
     {:error, {:invalid_enum_value, v}}
   end
-
   def(name_to_value(:dot_product)) do
     {:ok, 1}
   end
-
   def(name_to_value(:cross_product)) do
     {:ok, 2}
   end
-
   def(name_to_value(k)) do
     {:error, {:invalid_enum_name, k}}
   end
-
   def(value_to_name!(value)) do
     {:ok, name} = value_to_name(value)
     name
   end
-
   def(name_to_value!(name)) do
     {:ok, value} = name_to_value(name)
     value
   end
-
   def(meta(:names)) do
     [:dot_product, :cross_product]
   end
-
   def(meta(:values)) do
     [1, 2]
   end
-
   def(member?(1)) do
     true
   end
-
   def(member?(2)) do
     true
   end
-
   def(member?(_)) do
     false
   end
-
   def(name?(:dot_product)) do
     true
   end
-
   def(name?(:cross_product)) do
     true
   end
-
   def(name?(_)) do
     false
   end

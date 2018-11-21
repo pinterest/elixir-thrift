8ded4d6f96c1416441e330b310a91155fef7f2fc
Michael Oliver <thecodeboss@users.noreply.github.com>
2018-11-21 15:23:53 -0800

    Bump credo from 0.10.0 to 1.0.0 (#438)

--- mix.exs
+++ mix.exs
@@ -80,7 +80,7 @@ defmodule Thrift.Mixfile do
       # Development
       {:ex_doc, "~> 0.19", only: :dev, runtime: false},
       {:excoveralls, "~> 0.9", only: :test, runtime: false},
-      {:credo, "~> 0.10", only: :dev, runtime: false},
+      {:credo, "~> 1.0", only: :dev, runtime: false},
       {:dialyxir, "~> 0.5", only: :dev, runtime: false},
 
       # Runtime

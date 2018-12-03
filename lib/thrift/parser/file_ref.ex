1fb1a83c7bb60466c9b5efb2d1dbf2a52c05d7f2
Jon Parise <jon@pinterest.com>
2018-12-03 09:58:54 -0800

    Remove "include name" concept from FileRef (#443)
    First, remove FileRef.include_name/1 and the struct field of the same
    name. These were really never being used. Instead, we can construct the
    ParsedFile.name field value directly.
    
    Also, remove the FileRef.thrift_include type and just use Path.t() in its
    place. This is also more correct than the former's String.t() spec.

--- lib/thrift/parser/file_ref.ex
+++ lib/thrift/parser/file_ref.ex
@@ -1,19 +1,14 @@
 defmodule Thrift.Parser.FileRef do
   @moduledoc false
 
-  @type thrift_include :: String.t()
-  @type t :: %__MODULE__{path: String.t(), include_name: String.t(), contents: String.t()}
-  defstruct path: nil, include_name: nil, contents: nil
+  @type t :: %__MODULE__{path: Path.t(), contents: String.t()}
+  defstruct path: nil, contents: nil
 
   def new(path) do
     # We include the __file__ here to hack around the fact that leex and yecc don't
     # operate on files and lose the file info. This is relevant because the filename is
     # turned into the thrift module, and is necessary for resolution.
     thrift_file = File.read!(path) <> "\n__file__ \"#{path}\""
-    %__MODULE__{path: path, include_name: include_name(path), contents: thrift_file}
-  end
-
-  def include_name(path) do
-    Path.basename(path, ".thrift")
+    %__MODULE__{path: path, contents: thrift_file}
   end
 end
